4fac49e74cc02ba3e521c0df0397f896532ab8bf
Daniele410 <miraglia.daniele410@gmail.com>
2025-02-04 14:02:34 +0100

    Implement token-based login and logging

--- src/main/java/com/danozzo/springsecurityjwt/configuration/SpringSecurityConfig.java
+++ src/main/java/com/danozzo/springsecurityjwt/configuration/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.danozzo.springsecurityjwt.configuration;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,32 +12,39 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 public class SpringSecurityConfig {
-    private String jwtKey = "0nSIlpUfal/npfnmSbZrulW+Hatt9KcX48Tdsg6qBnr9q/ds2A3mFkbmLt5fs2NW";
+    //TODO hard-coded all secret key
+    private String jwtKey = "b025b762c366eb38ce9dafbebb86bda594cfb406c6e2b802058d7c9f0383b6be27ba15fc2823ee7d4258d2096da46baa3461c0adb283dfac4fe0b458f31fecb4c4d534845e0abf9bcc89fbd64809e28ce6fe760eacf2b3a768e0b2fd40aaccaf89f637eaac86c3866b1a0a7ef1235af5ff9abec7269659b886dc769f9f78cfccc03395d60b5f1bb309d162b9d0eae4b4e292e2dce2d5a5946582a6cbf1e6290b096c78ec4766363dbee95ad587000c69a3ff3afb474886c51e6bb3431f66287f07cabab24e772aac05f43767c4fff2e4b552bb829160299f426780fde2cb482b6a5cbce99be774ecacb4395865be8f1414d1f59a4ec4afd3cc507053f4432d6b";
 
     @Bean
-    public JwtDecoder jwtDecoder() {
-        SecretKeySpec secretKey = new SecretKeySpec(this.jwtKey.getBytes(), 0,
-                this.jwtKey.getBytes().length, "RSA");
-        return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
-    }
-
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
                 .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKey = new SecretKeySpec(this.jwtKey.getBytes(), 0, this.jwtKey.getBytes().length, "RSA");
+        return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(this.jwtKey.getBytes()));
     }
 
     @Bean
---
+++ src/main/java/com/danozzo/springsecurityjwt/controllers/LoginController.java
@@ -0,0 +1,22 @@
+package com.danozzo.springsecurityjwt.controllers;
+
+import com.danozzo.springsecurityjwt.services.JWTService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+public class LoginController {
+
+    @Autowired
+    public JWTService jwtService;
+
+    @PostMapping("/login")
+    public String getToken(Authentication authentication) {
+        String token = jwtService.generateToken(authentication);
+        return token;
+    }
+}
